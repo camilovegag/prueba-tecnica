39de25e03c2bcb37cb996d718147703eb9928404
camilovegag <camilovegag@unisabana.edu.co>
2021-02-23 16:27:54 -0500

    Create Background component ✨

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Background from "./components/Background";
 
 const App = () => {
-  return <h1>Hola mundo!</h1>;
+  return (
+    <div>
+      <Background />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Background/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./index.module.css";
+
+const Background = () => {
+  return (
+    <div>
+      <div className={styles["some-style"]} />
+      <div className={styles["filter"]}></div>
+    </div>
+  );
+};
+
+export default Background;
