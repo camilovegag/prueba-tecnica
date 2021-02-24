84a4e6cbe8f8f880ff301c7ab52eed174b23d167
camilovegag <camilovegag@unisabana.edu.co>
2021-02-24 15:15:55 -0500

    Create back icon 📍

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import Background from "./components/Background";
 import RoundedCard from "./components/RoundedCard";
+import { ArrowLeftOutlined } from "@ant-design/icons";
 
 const App = () => {
   return (
     <div>
+      <ArrowLeftOutlined className="icon" />
       <RoundedCard />
       <Background />
     </div>
